ae8ae4f82f303322785b26b64dc85de3b0a7161a
SrMarques0 <87779285+SrMarques0@users.noreply.github.com>
2022-07-13 18:51:51 -0300

    Here are we back again

---
+++ src/test/java/com/devS/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devS.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
